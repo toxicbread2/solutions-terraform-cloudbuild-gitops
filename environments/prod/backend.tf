4b3ff4db77c1a8d29aadc1a3fe79110ffd6ff758
toxicbread2 <toxicbread2@gmail.com>
2022-10-16 01:02:19 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "concise-lambda-364713-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="concise-lambda-364713"
